0572c4af75846f89da9488adfe31678636cc08c2
aman0179 <82507938+aman0179@users.noreply.github.com>
2023-08-07 15:00:47 +0530

    Update azure_instance_metadata.sh

--- azure_instance_metadata.sh
+++ azure_instance_metadata.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 metadata_json=$(curl -s -H "Metadata: true" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
-echo "$metadata_json"
+vm_size=$(echo "$metadata_json" | jq -r '.compute.vmSize')
+ 
+echo "Virtual Machine Size: $vm_size"
+
